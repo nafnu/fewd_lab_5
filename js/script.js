d00c7f5a4bbe2491773bc24edcfcf04d2246337c
nafnu <21520@student.dorset-college.ie>
2021-03-02 15:17:41 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ var hotel = {
     booked: 25,
     breakfast: 'Free Buffet Breakfast',
     gym: '24/7 Gym',
+    message: 'Hot stones massage',
+    meditation: 'Yoga at sunset',
     pool: 'Pools',
     roomTypes: ['double', 'twin', 'suite', 'king'],
     checkAvailablity: function() {
@@ -23,6 +25,16 @@ elPool.textContent = hotel.pool;
 
 var elRooms = document.getElementById('rooms');
 elRooms.textContent = hotel.checkAvailablity();
+
+var elBreakfast = document.getElementById("breakfast");
+elBreakfast.textContent = hotel.breakfast;
+
+var elRelax = document.getElementById("relax");
+elRelax.textContent = hotel.message;
+
+var elActivities = document.getElementById("activities");
+elActivities.textContent = hotel.meditation;
+
 
 elName.style.color = "turquoise"
 elNAme.style.font = "italic bold 20px arial,serif"
